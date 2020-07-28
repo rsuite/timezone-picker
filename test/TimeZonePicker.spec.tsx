427296ca8b78af7fba649432ef7e79e8b5516c7a
leihan <han.lei@mail.hypers.com>
2020-07-28 16:48:45 +0800

    test: 💍 补充测试用例

---
+++ test/TimeZonePicker.spec.tsx
@@ -0,0 +1,72 @@
+import { cleanup, fireEvent, render, waitFor } from '@testing-library/react';
+import * as React from 'react';
+import TimeZonePicker from '../src';
+import { pick } from 'lodash';
+
+describe('TimeZonePicker', () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  it('should be true always', function () {
+    expect(true).toBeTruthy();
+  });
+
+  it('should select first item', function () {
+    const { container, baseElement } = render(<TimeZonePicker defaultOpen virtualized={false} />);
+
+    const firstItem = baseElement.querySelector('.timezone-picker-menu-item');
+    expect(firstItem).not.toBeNull();
+    expect(firstItem.firstChild).toMatchInlineSnapshot(`
+      <div>
+        Abidjan
+      </div>
+    `);
+    fireEvent.click(firstItem.firstChild);
+    expect(container.querySelector('.rs-picker-toggle-value').firstChild as HTMLElement)
+      .toMatchInlineSnapshot(`
+      <div>
+        <i
+          class="timezone-picker-placeholder-icon rs-icon rs-icon-globe2"
+        />
+        Africa/Abidjan
+      </div>
+    `);
+  });
+
+  it('should onChange Callback Call', function (done) {
+    const handleChange = (value: string) => {
+      expect(value).toEqual('Asia/Tokyo');
+      done();
+    };
+    const { container, baseElement, queryByText } = render(
+      <TimeZonePicker defaultOpen virtualized={false} onChange={handleChange} />
+    );
+    fireEvent.click(queryByText('Tokyo'));
+  });
+
+  it('should onSelect Callback Call', function (done) {
+    const handleSelect = (value: string, item) => {
+      expect(pick(item, ['timezone', 'continent', 'name'])).toEqual({
+        timezone: 'Asia/Tokyo',
+        continent: 'Asia',
+        name: 'Tokyo',
+      });
+      expect(value).toEqual('Asia/Tokyo');
+      done();
+    };
+    const { container, baseElement, queryByText } = render(
+      <TimeZonePicker defaultOpen virtualized={false} onSelect={handleSelect} />
+    );
+    fireEvent.click(queryByText('Tokyo'));
+  });
+
+  it('should disabled continent group', function () {
+    const { container, baseElement, queryAllByRole } = render(
+      <TimeZonePicker defaultOpen virtualized={false} disableContinentGroup />
+    );
+    const listItems = queryAllByRole('listitem');
+    expect(listItems.length).toBeGreaterThanOrEqual(1);
+    expect(listItems[0].getAttribute('class')).not.toEqual('rs-picker-select-menu-group');
+  });
+});
